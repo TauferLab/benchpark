939c29fdb87c672b0f8b7bc9ba6481672c8f3f75
Ian Lumsden <lumsden.ian@gmail.com>
2024-06-17 14:41:56 -0700

    Adds pass-through variants to the a4md-benchmark package

--- repo/a4md-benchmark/package.py
+++ repo/a4md-benchmark/package.py
@@ -8,10 +8,16 @@ class A4mdBenchmark(CachedCmakePackage):
     
     version("main", branch="main")
     
+    variant("caliper", default=False)
+    variant("dyad", default=False)
+    
     depends_on("mpi", type=("build", "link"))
     depends_on("nlohmann-json", type="link")
     depends_on("fmt", type="link")
     depends_on("cli11", type="link")
     depends_on("a4md-core@main", type="link")
-    depends_on("a4md-orchestration@main", type="link")
+    depends_on("a4md-orchestration@main log_level=none", type="link")
+    
+    depends_on("a4md-core@main +dyad", when="+dyad")
+    depends_on("a4md-core@main +caliper", when="+caliper")
     
