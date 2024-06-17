6f5f021b1a79839cd291e2499de56a6b9212a750
Ian Lumsden <lumsden.ian@gmail.com>
2024-06-17 15:33:59 -0700

    Fixes variant bug in a4md-benchmark recipe

--- repo/a4md-benchmark/package.py
+++ repo/a4md-benchmark/package.py
@@ -15,8 +15,8 @@ class A4mdBenchmark(CachedCMakePackage):
     depends_on("nlohmann-json", type="link")
     depends_on("fmt", type="link")
     depends_on("cli11", type="link")
-    depends_on("a4md-core@main", type="link")
-    depends_on("a4md-orchestration@main log_level=none", type="link")
+    depends_on("a4md-core@main log_level=none", type="link")
+    depends_on("a4md-orchestration@main", type="link")
     
     depends_on("a4md-core@main +dyad", when="+dyad")
     depends_on("a4md-core@main +caliper", when="+caliper")
