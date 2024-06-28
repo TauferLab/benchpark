fffb5e3e13f039015f03ed13e213cc9f49896910
Ian Lumsden <lumsden.ian@gmail.com>
2024-06-28 13:42:19 -0700

    Updates a4md-benchmark based on changes to a4md-core recipe

--- repo/a4md-benchmark/package.py
+++ repo/a4md-benchmark/package.py
@@ -8,8 +8,9 @@ class A4mdBenchmark(CachedCMakePackage):
     
     version("main", branch="main")
     
+    core_plugins_values = ("mpi", "filesystem", "dyad")
+    variant("core_plugins", default="mpi,filesystem", values=core_plugins_values, multi=True)
     variant("caliper", default=False)
-    variant("dyad", default=False)
     
     depends_on("mpi", type=("build", "link"))
     depends_on("nlohmann-json", type="link")
@@ -17,7 +18,11 @@ class A4mdBenchmark(CachedCMakePackage):
     depends_on("cli11", type="link")
     depends_on("a4md-core@main log_level=none", type="link")
     depends_on("a4md-orchestration@main", type="link")
+
+    depends_on("a4md-core +caliper", when="+caliper")
     
-    depends_on("a4md-core@main +dyad", when="+dyad")
-    depends_on("a4md-core@main +caliper", when="+caliper")
+    # Use a for loop here since there's currently no way to easily propagate
+    # a4md-benchmark's core_plugins variant to a4md-core's plugins variant
+    for cpv in core_plugins_values:
+        depends_on("a4md-core plugins={}".format(cpv), when="core_plugins={}".format(cpv))
     
