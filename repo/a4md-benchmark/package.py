5a9da2079f9bb39e52b38de0bf50a55b01aa9c3b
Ian Lumsden <lumsden.ian@gmail.com>
2024-06-30 12:33:26 -0700

    Removes requirement of a4md-core log-level=none from a4md-benchmark

--- repo/a4md-benchmark/package.py
+++ repo/a4md-benchmark/package.py
@@ -15,7 +15,7 @@ class A4mdBenchmark(CachedCMakePackage):
     depends_on("mpi", type=("build", "link"))
     depends_on("nlohmann-json", type="link")
     depends_on("fmt", type="link")
-    depends_on("a4md-core@main log_level=none", type="link")
+    depends_on("a4md-core@main", type="link")
     depends_on("a4md-orchestration@main", type="link")
 
     depends_on("a4md-core +caliper", when="+caliper")
