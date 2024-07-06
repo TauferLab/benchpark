069e37ae4fd5340df57ad2b0e137b08e91d361d5
Ian Lumsden <lumsden.ian@gmail.com>
2024-07-05 19:58:27 -0700

    Adds v0.1.0 to a4x-orchestration

--- repo/a4x-orchestration/package.py
+++ repo/a4x-orchestration/package.py
@@ -6,8 +6,11 @@ class A4xOrchestration(CachedCMakePackage):
     homepage = "https://analytics4md.org/"
     # TODO: change to HTTPS URL once repo is public
     git      = "git@github.com:Analytics4MD/a4x-orchestration.git"
+
+    maintainers("ilumsden")
     
     version("main", branch="main")
+    version("0.1.0", tag="v0.1.0")
     
     depends_on("mpi", type=("build", "link"))
     depends_on("nlohmann-json", type="link")
